70955cb397792841ed43f8844747c7345ff24852
Alan Cashman <nocabbage@gmail.com>
2023-03-19 14:51:33 -0700

    trigger closeModal on outside click

--- src/pages/UploadPage.jsx
+++ src/pages/UploadPage.jsx
@@ -1,17 +1,27 @@
 import UploadForm from "../components/UploadForm/UploadForm";
 import Modal from "../components/ModalWindow/ModalWindow";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function UploadPage() {
-  let navigate = useNavigate();
-
   const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    if (showModal) {
+      window.addEventListener("click", handleCloseModal);
+    }
+    return () => {
+      window.removeEventListener("click", handleCloseModal);
+    };
+  }, [showModal, handleCloseModal]);
+
+  let navigate = useNavigate();
 
   function handleCloseModal() {
     setShowModal(false);
     navigate("/");
   }
+
   return (
     <div>
       <UploadForm
